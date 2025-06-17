8515fb97b0458802543d884bf6f355e4c918dc62
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-18 00:39:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	let len = str.length;
+	let result = "";
+	for(let i=0;i<l;i++){
+		result+=str[Math.floor(Math.random()*(len))];
+		
+	}
+	return result;
 }
 
 // Do not change the code below.
